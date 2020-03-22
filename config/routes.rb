831cf3709bb2ffab5dda1629979382271b7957ae
Adam Kossakowski <adam.kossakowski@outlook.com>
2020-03-22 11:47:52 +0100

    Customized portfolio show

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
+  root to: 'pages#home'
+
   get 'about', to: "pages#about"
   get 'contact', to: "pages#contact"
 
   resources :blogs
-  resources :portfolios
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
-  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
