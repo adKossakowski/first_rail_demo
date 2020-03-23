de5eda39b740203d4f8cc392cf17331afde8f359
Adam Kossakowski <adam.kossakowski@outlook.com>
2020-03-23 20:31:35 +0100

    Concern for generating imgge

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+  includes Placeholder
 
   validates_presence_of :title, :body, :main_image, :thumb_image
 
@@ -11,8 +12,8 @@ class Portfolio < ApplicationRecord
   after_initialize :set_defaults
 
   def set_defaults
-    self.main_image ||= "http://placehold.it/600x400"
-    self.thumb_image ||= "http://placehold.it/300x200"
+    self.main_image ||= Placeholder.image_generator(600, 400)
+    self.thumb_image ||= Placeholder.image_generator(300, 200)
   end
 
 end
